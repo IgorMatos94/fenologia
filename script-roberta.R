183e7f00c10f010dbc29d8f00d08e8110be0cb20
carinamotta <carinaisabellamotta@gmail.com>
2023-06-13 15:23:11 -0300

    load packages and data

--- script-roberta.R
+++ script-roberta.R
@@ -1,1 +1,44 @@
-abd
+# Fenologia 
+# 13 de junho 2023
+# Hades & Carina
+
+#1 LOAD PACKAGES----------------------------------------------------------------
+
+# a vector listing package names needed for importing the DNA sequences,
+#calculating genetic distance, calculated geographic distance, and performing
+#a Mantel test
+
+package.list <- c("activity", #usei esse pacote...
+                  "circular", 
+                  "phenology", 
+                  "lubridate",
+                  "ggplot2",
+                  "circular",
+                  'CircStats',
+                  "phenocamr",
+                  "lubridate",
+                  "bpnreg",
+                  "here"
+)
+
+#installing the packages if they aren't already on the computer
+new.packages <- package.list[!(package.list %in% installed.packages()
+                               [,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+#and loading the packages into R with a for loop
+for(i in package.list){library(i, character.only = T)}
+
+
+##LOAD DATA###
+
+load(here("modulos_pri", "dados", "Aula3 (1).RData"))
+
+
+##EXERCICIOS####
+
+#1
+
+
+
+
