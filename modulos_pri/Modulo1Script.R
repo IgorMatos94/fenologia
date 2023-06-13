b98548bcf03bb0904367889e93c6e83fdb4581c8
IgorMatos94 <igor.matos@unesp.br>
2023-06-13 15:04:16 -0300

    update

--- modulos_pri/Modulo1Script.R
+++ modulos_pri/Modulo1Script.R
@@ -1,11 +1,12 @@
 ### Script para Aula 1 - Curso de Introducao ao R ###
 ##################### --SETS--  #####################
-rm(list=ls())
+
 getwd()
-setwd("/Users/priscila/Desktop/2022.1/Pheno.22/Aula_1")
-load('Modulo1.RData')
-#load(file.choose())
-#save(laranjas, exemplo, file="Modulo1.RData")
+library(here)
+
+load.csv("modulos_pri", "dados", "laranjas.csv")
+
+??load.csv
 
 library(ggplot2)
 laranbox = 
